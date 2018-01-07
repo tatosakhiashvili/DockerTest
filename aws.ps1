f206eb8bb8bc151a2de864f0dd6fb46a8f4885e6
tatosakhiashvili <tato.sakhiashvili@gmail.com>
2018-01-07 23:17:31 +0400

    Add files via upload

--- aws.ps1
+++ aws.ps1
@@ -1,2 +1,2 @@
-Invoke-Expression -Command (aws ecr get-login --no-include-email)
-docker push 528855637444.dkr.ecr.eu-west-1.amazonaws.com/test
+Invoke-Expression -Command (aws ecr get-login --no-include-email --region eu-west-1)
+docker push 528855637444.dkr.ecr.eu-west-1.amazonaws.com/test:latest
